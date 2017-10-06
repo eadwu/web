f1332328b24e4e3ccf72778b951ed67a54d6440c
eadwu <fangkazuto@gmail.com>
2017-10-05 20:49:00 -0400

    Conform to criteria
    Reset[ Game] button

--- assignments/level-1/hot-and-cold/index.js
+++ assignments/level-1/hot-and-cold/index.js
@@ -1,94 +1,136 @@
-var input = "";
+var randomNumber;
 
-var number;
-var degree;
-var tableElement;
-var statusElement;
-var displayElement;
+var resetButton;
+var outputTable;
+var displayInput;
 
-var activeGame = true;
+var tries = 5;
+var userInput = "";
+var rangeDegree = "";
+
+function setDegree ()
+{
+  const displacement = Math.abs(randomNumber - userInput);
+
+  if (displacement === 0)
+  {
+    endGame(true);
+    // Prevent extra guesses at endgame
+    tries = 0;
+  }
+  else if (displacement <= 5)
+  {
+    rangeDegree = "Very Hot";
+    tries++;
+  }
+  else if (tries === 0)
+  {
+    endGame(false);
+  }
+  else if (displacement <= 8)
+  {
+    rangeDegree = "Hot";
+  }
+  else if (displacement <= 15)
+  {
+    rangeDegree = "Very Warm";
+  }
+  else if (displacement <= 20)
+  {
+    rangeDegree = "Warm";
+  }
+  else if (displacement <= 30)
+  {
+    rangeDegree = "Cool";
+  }
+  else if (displacement <= 40)
+  {
+    rangeDegree = "Very Cool";
+  }
+  else if (displacement <= 55)
+  {
+    rangeDegree = "Cold";
+  }
+  else if (displacement > 55)
+  {
+    rangeDegree = "Very Cold";
+  }
+}
+
+function endGame (trueEnd)
+{
+  if (trueEnd)
+    rangeDegree = "You Win!";
+  else
+    rangeDegree = `You Lose! The number was ${randomNumber}.`;
+
+  resetButton.textContent = "Reset Game";
+}
+
+function resetUserInput ()
+{
+  userInput = "";
+  displayInput.textContent = "";
+}
 
 function display ()
 {
-  const entry = tableElement.insertRow();
+  if (tries < 0) return;
+  const entry = outputTable.insertRow();
   const entryGuess = entry.insertCell();
   const entryStatus = entry.insertCell();
 
-  if (!activeGame)
-  {
-    degree = "";
-    statusElement.textContent = "You win!";
-  }
+  entryGuess.textContent = userInput;
+  entryStatus.textContent = rangeDegree;
 
-  entryGuess.textContent = input;
-  entryStatus.textContent = degree;
-
-  input = "";
-  displayElement.textContent = "";
-}
-
-function getStatus ()
-{
-  const displacement = Math.abs(number - input);
-
-  if (displacement === 0)
-    activeGame = false;
-  else if (displacement <= 5)
-    degree = "Very Hot";
-  else if (displacement <= 8)
-    degree = "Hot";
-  else if (displacement <= 15)
-    degree = "Very Warm";
-  else if (displacement <= 20)
-    degree = "Warm";
-  else if (displacement <= 30)
-    degree = "Cool";
-  else if (displacement <= 40)
-    degree = "Very Cool";
-  else if (displacement <= 55)
-    degree = "Cold";
-  else if (displacement > 55)
-    degree = "Very Cold";
-  else
-    degree = "";
+  resetUserInput();
 }
 
 document.addEventListener("DOMContentLoaded", () =>
 {
-  let tries = 5;
-  const inputButtons = document.querySelectorAll("div button");
+  // parseInt rounds down; Math.floor does the same; add 0.5 to fix rounding problems
+  randomNumber = Math.floor(Math.random() * 100 + 0.5);
+
+  resetButton = document.querySelector("#reset");
+  outputTable = document.querySelector("#data_table");
+  displayInput = document.querySelector("#input");
+
+  const fakeNumberPad = document.querySelectorAll("div button");
   const submitGuess = document.querySelector("#submit_guess");
 
-  number = Math.floor(Math.random() * 100 + 0.5);
-  tableElement = document.querySelector("#data_table");
-  displayElement = document.querySelector("#input");
-  statusElement = document.querySelector("#game_status");
-
-  for (let index = 0; index < inputButtons.length; index++)
+  for (let index = 0; index < fakeNumberPad.length; index++)
   {
-    const inputButton = inputButtons[index];
+    const padNumberButton = fakeNumberPad[index];
 
-    inputButton.addEventListener("click", () =>
+    padNumberButton.addEventListener("click", () =>
     {
-      input = input + inputButton.textContent;
-      displayElement.textContent = input;
+      userInput = userInput + padNumberButton.textContent;
+      displayInput.textContent = userInput;
     });
   }
 
+  resetButton.addEventListener("click", () =>
+  {
+    resetUserInput();
+    if (resetButton.textContent === "Reset Game")
+    {
+      // lazy bum way
+      // window.location.reload();
+      tries = 5;
+      resetButton.textContent = "Reset";
+      randomNumber = Math.floor(Math.random() * 100 + 0.5);
+
+      const outputTableData = outputTable.querySelector("tbody");
+      const outputTableEntries = outputTableData.childNodes.length - 1;
+      for (let child = 0; child < outputTableEntries; child++)
+        outputTableData.removeChild(outputTableData.lastChild);
+    }
+  });
+
   submitGuess.addEventListener("click", () =>
   {
-    tries = tries - 1;
-
-    if (activeGame)
-    {
-      getStatus();
-      display();
-
-      if (tries === 0 && activeGame)
-      {
-        activeGame = false;
-        statusElement.textContent = "You lose!";
-      }
-    }
+    tries--;
+    setDegree();
+    display();
   });
 });
