9533397b5bc97429a06bc55f93259a6c398ad202
eadwu <fangkazuto@gmail.com>
2017-12-09 10:50:19 -0500

    Add utilities declaration file

---
+++ typings/utilities.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for utilities.ts
+// Project: https://gitlab.com/bt-wcsp/web
+// Definitions by: Edmund Wu <https://gitlab.com/eadwu>
+
+declare function getRandomInteger (min: number, max: number): number;
+declare function getOrdinalSuffix (num: number): string;
