0e10f3d21f728d462949c2db62009e23bd733eb8
eadwu <fangkazuto@gmail.com>
2017-12-11 21:02:39 -0500

    Keep tranposed js simple

--- assignments/level-3/black-and-red/js/player.ts
+++ assignments/level-3/black-and-red/js/player.ts
@@ -1,40 +1,30 @@
-interface PlayerParam
-{
-  lastStrategy?: number;
-  redTokens?: number;
-  blackTokens?: number;
-}
-
 class Player
 {
-  _params: PlayerParam;
-  constructor (params?: PlayerParam)
+  redTokens: number;
+  blackTokens: number;
+  constructor ()
   {
-    this._params = {
-      lastStrategy: undefined,
-      redTokens: 10,
-      blackTokens: 10,
-      ...params
-    };
+    this.redTokens = 10;
+    this.blackTokens = 10;
   }
 
   getRedTokens ()
   {
-    return this._params.redTokens;
+    return this.redTokens;
   }
 
   getBlackTokens ()
   {
-    return this._params.blackTokens;
+    return this.blackTokens;
   }
 
   setRedTokens (amount)
   {
-    this._params.redTokens += amount;
+    this.redTokens += amount;
   }
 
   setBlackTokens (amount)
   {
-    this._params.blackTokens += amount;
+    this.blackTokens += amount;
   }
 }
