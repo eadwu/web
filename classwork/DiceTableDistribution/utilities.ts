ebca436c8e22000052d8a856c2a88d8240aa38e9
eadwu <fangkazuto@gmail.com>
2017-12-12 19:28:58 -0500

    Add classwork `DiceTableDistribution`
    Assigned Dec 6, 2017

---
+++ classwork/DiceTableDistribution/utilities.ts
@@ -0,0 +1,34 @@
+function getRandomInteger (min: number, max: number): number
+{
+  return parseInt(Math.random() * (max + 1 - min), 10) + min;
+}
+
+function getOrdinalSuffix (num: number): string
+{
+  const mod10 = num % 10;
+  const mod100 = num % 100;
+
+  if (mod10 === 1 && mod100 !== 11)
+    return "st";
+
+  if (mod10 === 2 && mod100 !== 12)
+    return "nd";
+
+  if (mod10 === 3 && mod100 !== 13)
+    return "rd";
+
+  return "th";
+}
+
+function forNDo (n: number, func: (index: number) => any): void
+{
+  for (let index = 0; index < n; index++)
+    func(index);
+}
+
+function mapRange (n: number, func: (index: number) => any): any[]
+{
+  const result = [];
+  forNDo(n, index => result.push(func(index)));
+  return result;
+}
