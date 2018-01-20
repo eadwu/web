d74350f539443badaf756abf3b23b5256aa013f0
eadwu <fangkazuto@gmail.com>
2018-01-19 22:15:04 -0500

    Make pathfinding more `properly`

--- assignments/fall-final-project/assets/js/pathfinding.ts
+++ assignments/fall-final-project/assets/js/pathfinding.ts
@@ -1,15 +1,16 @@
 /// <reference path="../../../../typings/p5.d.ts" />
+type ArrayLike = Array<any> | NodeListOf<any>;
+
 (() =>
 {
-  const _window = this;
-
   function getRandomInteger (min: number, max: number): number
   {
-    return parseInt(Math.random() * (max + 1 - min), 10) + min;
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
   }
 
-  function loop (arr, func)
+  function loop (arr: ArrayLike<any>, func: (value: any, index: number, source: ArrayLike<any>) => void)
   {
+    const transformedArr = Array.isArray(arr) ? arr : Array.from(arr);
     for (let i = 0; i < arr.length; i++)
       func(arr[ i ], i, arr);
   }
@@ -19,21 +20,30 @@
     const container = document.querySelector(".cd-HardFix_Align") as HTMLElement;
     const inputElements = document.querySelectorAll("input");
 
-    const modifiers = [
-      [ -1, -1 ],
+    const defaultModifiers = [
       [ 0, -1 ],
       [ -1, 0 ],
-      [ 0, 1 ],
       [ 1, 0 ],
-      [ 1, 1 ]
+      [ 0, 1 ]
     ];
 
+    const diagonalModifiers = [
+      [ -1, -1 ],
+      [ -1, 1 ],
+      [ 1, 1 ],
+      [ 1, -1 ]
+    ];
+
+    const params = location.search.substr(1).split("=");
+    let modifiers = params.indexOf("allowDiagonals") >= 0 ? defaultModifiers.concat(diagonalModifiers) : defaultModifiers;
+
     const types = {
-      SPACE: 0,
-      ORIGINPOINT: 1,
-      ENDPOINT: 2,
+      ORIGINPOINT: 0,
+      ENDPOINT: 1,
+      SPACE: 2,
       WALL: 3,
-      PATH: 4
+      CHECKED: 4,
+      PATH: 5
     };
 
     const root = new p5(base =>
@@ -45,12 +55,14 @@
         x: number;
         y: number;
 
-        constructor (x, y)
+        constructor (x: number, y: number)
         {
           this.x = x;
           this.y = y;
-          this.g = Math.abs(endX - x) + Math.abs(endY - y);
-          // this.g = Math.sqrt((endX - x) ** 2 + (endY - y) ** 2);
+          // Manhanttan Distance
+          // this.g = Math.abs(endX - x) + Math.abs(endY - y);
+          // Euclidean Distance
+          this.g = Math.sqrt((endX - x) ** 2 + (endY - y) ** 2);
 
           if (`${x},${y}` === start)
             this.type = types.ORIGINPOINT;
@@ -62,26 +74,31 @@
 
         next ()
         {
-          let next = { type: 0, g: 1000 };
+          let current;
+
           loop(modifiers, mod =>
           {
-            const [ modX, modY ] = mod;
+            const [ modY, modX ] = mod;
             const newX = this.x + modX;
             const newY = this.y + modY;
 
-            if (newX >= 0 && newY >= 0 && newX < rows && newY < columns)
+            if (newX >= 0 && newX < length && newY >= 0 && newY < length)
             {
-              const targetNode = grid[ newX ][ newY ];
-              if (targetNode.type === types.SPACE && targetNode.g < next.g)
-                next = targetNode;
-              else if (targetNode.type === types.ENDPOINT)
-                next = true;
+              const targetNode = grid[ newY ][ newX ];
+
+              // Ensure type is either Space, Checked, or Path
+              // Should only have to ensure the type is Space
+              // if (targetNode.type > 0 && (targetNode.type % 2 === 0 || targetNode.type === 1 || targetNode.type === 5))
+              if (targetNode.type !== types.ORIGINPOINT && targetNode.type !== types.WALL)
+                current = current ?
+                  targetNode.g <= current.g ?
+                    targetNode :
+                    current :
+                  targetNode;
             }
           });
-          if (typeof (next) === "object")
-            next.type = types.PATH;
 
-          return next;
+          return current;
         }
 
         render ()
@@ -92,92 +109,134 @@
             base.fill("#f44336");
           else if (this.type === types.WALL)
             base.fill(0);
+          else if (this.type === types.CHECKED)
+            base.fill("#ffeb3b");
           else if (this.type === types.PATH)
-            base.fill("#03a9f4");
+            base.fill("#9c27b0");
           else
-            base.fill(207, 90, 0 + this.g / Math.sqrt(2 * 15 ** 2) * 100);
+            base.fill(207, 90, 0 + this.g / Math.sqrt(2 * length ** 2) * 100);
 
           base.stroke(0);
           base.rect(this.x * rowWidth, this.y * columnHeight, rowWidth, columnHeight);
         }
       }
 
-      const rows = 15;
-      const columns = 15;
+      const length = 15;
 
-      _window.startX = 0;
-      _window.startY = 0;
-
+      let startX = 0;
+      let startY = 0;
       let endX = 14;
       let endY = 14;
+
       let start = `${startX},${startY}`;
       let end = `${endX},${endY}`;
-      let grid = Array.apply(null, new Array(rows)).map((_, y) => Array.apply(null, new Array(columns)).map((_, x) => new Node(x, y)));
+      let grid = Array.apply(null, new Array(length)).map((_, y) => Array.apply(null, new Array(length)).map((_, x) => new Node(x, y)));
 
       let rowWidth;
       let columnHeight;
 
-      _window.updateEndpoints = () =>
+      function reCalculatePath ()
       {
-        start = `${startX},${startY}`;
-        end = `${endX},${endY}`;
-        resetGrid();
-      };
+        let cNode = grid[ startY ][ startX ];
+        let i = 0;
+
+        while (true)
+        {
+          const response = cNode.next();
+
+          if (response.type === types.ENDPOINT)
+          {
+            break;
+          }
+          else
+          {
+            cNode = response;
+            cNode.type = types.PATH;
+          }
+        }
+      }
+
+      function updateEndpoints ()
+      {
+        const newStart = `${startX},${startY}`;
+        const newEnd = `${endX},${endY}`;
+
+        if (newStart !== newEnd)
+        {
+          start = newStart;
+          end = newEnd;
+          resetGrid();
+        }
+      }
 
       function resetGrid ()
       {
-        grid = Array.apply(null, new Array(rows)).map((_, y) => Array.apply(null, new Array(columns)).map((_, x) => new Node(x, y)));
+        grid = Array.apply(null, new Array(length)).map((_, y) => Array.apply(null, new Array(length)).map((_, x) => new Node(x, y)));
+        grid[ startY ][ startX ].type = types.ORIGINPOINT;
+        grid[ endY ][ endX ].type = types.ENDPOINT;
+
+        reCalculatePath();
       }
 
       function updateDefaults ()
       {
-        rowWidth = container.clientWidth / rows;
-        columnHeight = container.clientHeight / columns;
+        rowWidth = container.clientWidth / length;
+        columnHeight = container.clientHeight / length;
       }
 
       base.setup = () =>
       {
         base.colorMode(base.HSB);
         base.createCanvas(container.clientWidth, container.clientHeight);
-        updateDefaults();
+        base.noLoop();
+
+        loop(inputElements, element =>
+        {
+          const coordinateState: string = element.getAttribute("data-state");
+          const coordinateDimension: string = element.getAttribute("data-target-type");
+
+          element.addEventListener("input", event =>
+          {
+            const originalValue = element.value;
+            const constrainedValue = originalValue ?
+              originalValue < 0 ?
+                length + ~~originalValue :
+                originalValue > length - 1 ?
+                  originalValue % length :
+                  +originalValue :
+              0;
+
+            coordinateState === "start" ?
+              coordinateDimension === "X" ?
+                startX = constrainedValue :
+                startY = constrainedValue :
+              coordinateDimension === "X" ?
+                endX = constrainedValue :
+                endY = constrainedValue;
+
+            element.value = constrainedValue;
+            updateEndpoints();
+
+            base.redraw();
+          });
+        });
+
+        base.windowResized();
       };
 
       base.draw = () =>
       {
         loop(grid, row => { loop(row, node => { node.render(); }); });
-
-        let cNode = grid[ startX ][ startY ];
-
-        while (true)
-        {
-          if (!cNode.next) return;
-          const response = cNode.next();
-          if (response === true)
-            break;
-          else
-            cNode = response;
-        }
+        reCalculatePath();
       };
 
       base.windowResized = () =>
       {
         base.resizeCanvas(container.clientWidth, container.clientHeight);
         updateDefaults();
+
+        base.redraw();
       };
     }, container);
-
-    loop(inputElements, element =>
-    {
-      const coordinateDimension = element.getAttribute("data-target-type");
-
-      element.addEventListener("keydown", event =>
-      {
-        if (event.key === "Enter")
-        {
-          _window[ `start${coordinateDimension}` ] = element.value || 0;
-          updateEndpoints();
-        }
-      });
-    });
   });
 }).call(this);
