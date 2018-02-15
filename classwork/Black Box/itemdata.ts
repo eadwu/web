ef9f9448aa20dc7ee506c36f7042c11a96d346bf
eadwu <fangkazuto@gmail.com>
2018-02-14 19:14:27 -0500

    Add homework assignment `Black Box`
    Due Feb 28, 2018

---
+++ classwork/Black Box/itemdata.ts
@@ -0,0 +1,8 @@
+NAME = 0;
+INFO = 1;
+IMAGE = 2;
+
+pageTitle = "Programming Paradigms";
+
+items =
+  "Object-Oriented Programming:Object-oriented programming (OOP) is a programming paradigm based on the concept of \"objects\", which may contain data, in the form of fields, often known as attributes, and code, in the form of procedures, often known as methods.:oop.webp;Functional Programming:In computer science, functional programming is a programming paradigm—a style of building the structure and elements of computer programs—that treats computation as the evaluation of mathematical functions and avoids changing-state and mutable data.:fp.png;Procedural Programming:Procedural programming is a programming paradigm, derived from structured programming, based upon the concept of the procedure call.:pp.png;Imperative Programming:In computer science, imperative programming is a programming paradigm that uses statements that change a program's state.:ip.jpg;Modular Programming:Modular programming is a software design technique that emphasizes separating the functionality of a program into independent, interchangeable modules, such that each contains everything necessary to execute only one aspect of the desired functionality.:mp.jpg;Event-Driven Programming:In computer programming, event-driven programming is a programming paradigm in which the flow of the program is determined by events such as user actions (mouse clicks, key presses), sensor outputs, or messages from other programs/threads.:edp.jpg";
